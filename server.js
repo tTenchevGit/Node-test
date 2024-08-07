910456ad749c08f0fcbfd9972f60914fa9129f95
Toni Tenchev <tonitenchev93@gmail.com>
2024-08-07 14:30:15 +0100

    start handlebars implementation

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
+
+
+
+
 const express = require('express');
+const path = require('path');
+const logger = require('./middlewares/logger');
+const checkMiddleware = require('./middlewares/moddleware');
+const exphbs = require('express-handlebars');
+
 const app = express();
 const portNumber = 5000;
-const path = require('path');
-const checkMiddleware = require('./middlewares/moddleware');
-const logger = require('./middlewares/logger');
-
 
 app.use(logger);
+app.use(express.static('public'));
 
-app.use(express.static('public')); 
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
-    console.log('Hello World');
-   
-}); 
+    res.render('home', { layout: false });
+});
 
 app.get('/download', (req, res) => {
-    const filePath = './views/home.html';
-    res.download(filePath, 'home.html', (err) => {
+    const filePath = './public/done.html';
+    res.download(filePath, 'done.html', (err) => {
         if (err) {
             console.log('Error downloading file:', err);
         } else {
@@ -27,10 +33,8 @@ app.get('/download', (req, res) => {
     });
 });
 
-
-
-app.get('/view', (req, res) => {
-    const filePath = path.join(__dirname, 'views', 'home.html');
+app.get('/public', (req, res) => {
+    const filePath = path.join(__dirname, 'public', 'done.html');
     res.sendFile(filePath, (err) => {
         if (err) {
             console.log('Error sending file:', err);
@@ -41,22 +45,16 @@ app.get('/view', (req, res) => {
     });
 });
 
-// app.get('/cats', (req, res)=>{
-//     res.send('Meow');
-//     console.log('Meow');
-// });
-
 app.post('/cats', (req, res) => {
     res.send('Run fast eeeee');
     console.log('Meow');
 });
 
-app.get('/cats/:id?',checkMiddleware , (req, res) => {
-    console.log(req.params); 
+app.get('/cats/:id?', checkMiddleware, (req, res) => {
+    console.log(req.params);
     const postId = req.params.id;
     res.send(`This is the ID ${postId}`);
     console.log(`Post ${postId}`);
-  });
-  
+});
 
-app.listen(portNumber, () => console.log(`Server is runing on port:${portNumber}`));
+app.listen(portNumber, () => console.log(`Server is running on port: ${portNumber}`));
