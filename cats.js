b9e646e2de8bbd931d82a63a5300c631dd20061b
Toni Tenchev <tonitenchev93@gmail.com>
2024-08-07 16:00:41 +0100

    added  a json file to collect cats data

--- cats.js
+++ cats.js
@@ -1,10 +1,18 @@
-const cats = [];
+const fs = require('fs');
+const catsData = require('./cats.json');
+const cats =  catsData.slice();
+const path = require('path');
 
 function add(name) {
  cats.push(name);
-  
+ fs.writeFile('cats.json', JSON.stringify(cats, null, 2), (err) => {
+    if (err) {
+      console.log('Error writing file:', err);
+    } else {
+      console.log('File written successfully');
+    }
+  });
 };
-
 
 function getAll() {
     return cats.slice();
